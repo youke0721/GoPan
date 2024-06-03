01f2f82c91e8675a22bdaa80e347f0ab0ace82ff
wuhaokai2 <1229570767@qq.com>
2024-06-03 12:34:53 +0800

    文件保存逻辑

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -25,5 +26,29 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == "POST" {
 		// 保存文件存储流到本地目录
 		// 添加实际的文件处理逻辑
+		file, head, err := r.FormFile("uploadFile")
+		if err != nil {
+			fmt.Printf("Failed to get data,err:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
+		newFile, err := os.Create("./tmp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create file,err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf(" Failed to save data inio file err:%s\n", err.Error())
+			return
+		}
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
+}
+
+// 上传成功
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished")
 }
